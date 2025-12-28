f652b551020e863d699a6249504b0b91712d7805
arukapega <dragon.night3579@gmail.com>
2025-12-28 14:18:27 +0900

    機能を実装した

---
+++ src/Assets/1 Lifegame/InitializeScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InitializeScript : MonoBehaviour
+{
+    [SerializeField] CustomRenderTexture tex = default!;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tex.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
